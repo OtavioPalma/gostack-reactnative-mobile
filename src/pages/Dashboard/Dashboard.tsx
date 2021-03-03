2714518f2e616025ae0f9883787cd82c17b9dde9
Otavio Palma <otaviopalma@gmail.com>
2021-03-03 17:20:15 -0300

    feat: sign out

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Button } from '../../components/Button/Button';
+import { useAuth } from '../../hooks/useAuth';
 
 export const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Dashboard</Text>
-      <Text>Dashboard</Text>
-      <Text>Dashboard</Text>
-      <Text>Dashboard</Text>
-      <Text>Dashboard</Text>
+
+      <Button onPress={signOut}>Sair!</Button>
     </View>
   );
 };
